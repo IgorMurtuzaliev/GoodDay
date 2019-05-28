fbe9575133bdead13777091b8ca7b248a4b6bff4
i.murtuzaliev <ingwarrior.99@yandex.ru>
2019-05-28 11:26:35 +0300

    did project layers

---
+++ GoodDay.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using GoodDay.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodDay.DAL.EF
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseLazyLoadingProxies();
+
+        //}
+
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<File> Files { get; set; }
+    }
+}
